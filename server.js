e1ec1bf3b63e888eb2e4c76e027e515e07aa4047
pero4l <lilpero03@gmail.com>
2025-09-19 13:27:07 +0100

    I am on the register

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require("express");
 
+const fs = require("fs")
+
 const app = express();
 
 app.use(express.json());
 
 const PORT = 7000;
+
+const FILE_PATH = require("./user.json")
+
+
+function readUsers(){
+    try{
+        if(!fs.existsSync(FILE_PATH)) return [];
+        const data = fs.readFileSync(FILE_PATH, "utf8");
+        return JSON.parse(data);
+    } catch (err) {
+        console.error("Erro reading users:", err)
+        return []; 
+    }
+}
+
+function writeUsers(user){
+    try{
+        fs.writeFileSync(FILE_PATH, JSON.stringify(user, null, 2));
+    } catch (err) {
+        console.log("Error writing users:", err);
+        
+    }
+}
 
 
 
@@ -13,6 +38,25 @@ app.get('/', (req, res)=>{
         success: true,
         message: "Welcome DevPTB the server is running"
     })
+});
+
+
+app.post("/register", (req, res) => {
+
+    const{name, email, password} = req.body
+
+    res.status(200).json({
+        success: true,
+        message: "Account created successfully"
+    })
+
+    const user = readUsers()
+
+    if(user.find((u)=> u.name === name && u.email === email)){
+        console.log("User already exist.");
+        
+    }
+
 })
 
 
