c027ee66a7d4d095b408e46da4214a69c29f3a5c
pero4l <lilpero03@gmail.com>
2025-09-20 23:28:29 +0100

    routed it and now want to register it

--- routers/auth.route.js
+++ routers/auth.route.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const {register, login} = require("../controllers/auth.controllers")
+const {createPost} = require("../controllers/ticket.controllers")
 
 
 router.post("/register", register)
 router.post("/login", login)
+
+router.post("/create-post", createPost)
 
 module.exports = router
