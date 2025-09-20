5018574d624a0b118715e1472a4a25376f9c6d0d
pero4l <lilpero03@gmail.com>
2025-09-20 23:21:50 +0100

    finish the ticket gen and want to test it now

--- controllers/ticket.controllers.js
+++ controllers/ticket.controllers.js
@@ -9,8 +9,12 @@ function createPost(req, res){
   const date = new Date().toLocaleString();
 
   if(title=== post.title && description === post.description && type === post.type && price === post.price){
-    re
+    return res.status(400).json({
+      success: false,
+      message: "Post already exist in the data base"
+    })
   }
+
 
 
     const newPost = {
@@ -22,5 +26,13 @@ function createPost(req, res){
         date
     }
 
+    post.push(newPost),
+    writePosts(post)
+
+    res.status(201).json({
+    success: true,
+    message: "Ticket generated successfully",
+    data: newPost
+  });
 
 }
